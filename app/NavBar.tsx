b096271274e478d1a076e6a28f4da1505863c7d1
Joseph Rivera <josephrivera@Josephs-MBP-2.lan>
2024-07-30 15:46:00 -0500

    style navbar active links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 import { AiFillBug } from "react-icons/ai";
+import classnames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname();
     const linkList = [
         {
             label: 'Dashboard',
@@ -12,7 +16,7 @@ const NavBar = () => {
             label: 'Issues',
             href: '/issues'
         }
-    ]
+    ];
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/">
@@ -22,7 +26,11 @@ const NavBar = () => {
             {linkList.map(link => 
                 <li key={link.href} >
                     <Link 
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zince-800 transition-colors': true
+                        })}
                         href={link.href}
                     >
                         {link.label}
